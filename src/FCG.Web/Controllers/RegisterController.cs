f3ae499dd7156cca07421e89600124251c3f4400
CrJunior08 <cleristoncontato@outlook.com>
2025-05-21 13:00:31 -0300

    Criado estrutura inicial do projeto

---
+++ src/FCG.Web/Controllers/RegisterController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FCG.Web.Controllers
+{
+    public class RegisterController : Controller
+    {
+    }
+}
---
+++ tests/FCG.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace FCG.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
