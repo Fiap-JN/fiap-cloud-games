8a65f2441c6b0e3ddd83a7de7516f939194130ec
Joao Pedro <joaopedro.contato829@gmail.com>
2025-05-24 12:58:26 -0300

    Update tela cadastro jogo (imcompleta ainda)

---
+++ src/FCG.Web/Models/Jogo.cs
@@ -0,0 +1,11 @@
+﻿namespace FCG.Web.Models
+{
+    public class Jogo
+    {
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public string Plataforma { get; set; }
+        public string Descricao { get; set; }
+    }
+
+}
